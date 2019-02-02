f64d069979be9981c574f499b13910f4f1e3d97b
LuisReyes98 <luisrogelio98@gmail.com>
2019-02-02 18:01:07 -0400

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D playerRgBody;
+    public float SPEED = 0.5f;
+    public float JUMP_SPEED = 100f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRgBody.velocity = new Vector2(Input.GetAxis("Horizontal") * SPEED,playerRgBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRgBody.AddForce(Vector2.up * JUMP_SPEED );
+            
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            //para que el salto se detenga cuando sueltes espacio 
+            playerRgBody.velocity = new Vector2(playerRgBody.velocity.x, 0.1f);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Unity Project of a platformer
+Made as part of the [Platzi Unity 2D Course](https://platzi.com/clases/unity-2d/) 
+
+Used Unity 2018.3.2f1
