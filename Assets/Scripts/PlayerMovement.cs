5e7f258e66b41ee6cbf03353e5b2c92f2e6c5155
LuisReyes98 <luisrogelio98@gmail.com>
2019-02-18 21:13:57 -0400

    Player animation and jumping mechanics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D playerRgBody;
     public float SPEED = 0.5f;
     public float JUMP_SPEED = 100f;
+
+    public Animator playerAnimator;
+
+    public float MAX_JUMP_ANGLE = -0.7f;
     bool isGrounded = true;
     bool stopJump = false;
     
@@ -20,18 +24,46 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // movimiento de caminar
         playerRgBody.velocity = new Vector2(Input.GetAxis("Horizontal") * SPEED,playerRgBody.velocity.y);
+        
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            playerAnimator.SetBool("isWalking",false);
+        }else if (Input.GetAxis("Horizontal") > 0)
+        {
+            playerAnimator.SetBool("isWalking",true);
 
+            // Por get component es mas lento
+            GetComponent<SpriteRenderer>().flipX = false;
+            // pero si es algo sencillo es mas facil que asignar una referencia
+
+
+        }else if (Input.GetAxis("Horizontal") < 0)
+        {
+            playerAnimator.SetBool("isWalking", true);
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+
+
+
+        //  movimiento de saltar
         if (isGrounded)//si esta en el piso
         {
+            playerAnimator.SetBool("isJumping", false);
             if (Input.GetKeyDown(KeyCode.Space))//preciona la tecla
             {
                 playerRgBody.AddForce(Vector2.up * JUMP_SPEED );
                 isGrounded = false;
                 stopJump = true;
+                playerAnimator.SetBool("isJumping",true);
 
-            }
-            
+            }            
+
+            // playerAnimator.SetTrigger("Jump");
+
+
         }else
         {
             if (stopJump)//para frenar el salto una vez
@@ -42,6 +74,7 @@ public class PlayerMovement : MonoBehaviour
                     // Debug.Log("Solataste saltar");
                     stopJump = false;
                     playerRgBody.velocity = new Vector2(playerRgBody.velocity.x, 0.1f);
+
                 }
                 
             }
@@ -59,10 +92,23 @@ public class PlayerMovement : MonoBehaviour
     /// <param name="collision">The Collision2D data associated with this collision.</param>
     void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true;
-            stopJump = false;
+            // Detectando la dirreción de la colision
+            Vector2 player = new Vector2(this.gameObject.transform.position.x,this.gameObject.transform.position.y);
+
+            Vector2 dir = collision.contacts[0].point - player;
+            float dirY = dir.normalized.y;
+            if (dirY < MAX_JUMP_ANGLE)//Si la colision fue desde la parte inferior del jugador aka suelo            
+            {
+                // Debug.Log(dirY);
+                isGrounded = true;
+                stopJump = false;
+                
+            }
         }
     }
+
+    
 }
