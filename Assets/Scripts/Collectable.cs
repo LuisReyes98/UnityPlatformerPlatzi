84aa26d7e60bb4756f9586bddd8fe2253ca0ebac
LuisReyes98 <luisrogelio98@gmail.com>
2019-05-13 02:15:55 -0400

    build ready

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -14,6 +14,7 @@ public class Collectable : MonoBehaviour
     // Start is called before the first frame update    
     void Start()
     {
+        collectableQuantity = 0;//de lo contrario el valor se conservaria entre escenas
         collectableText = GameObject.Find("CollectableQuantityText").GetComponent<Text>();
         
         // collectableText = GameObject.FindObjectOfType(Text);// esto puede dar error si hay mas de un objeto con el mismo tipo de dato
--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -9,6 +9,10 @@ public class SceneChanger : MonoBehaviour
     public static void ChangeSceneTo(string sceneName){
         SceneManager.LoadScene(sceneName);
     }
+    public void ChangeCurrentSceneTo(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 
     public static void NextScene(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
